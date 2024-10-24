313c242a444b3d542f9ec567033c22f0f530a1d6
Vinicius <viniciusedward0307@gmail.com>
2024-10-24 17:49:20 +0000

    salvando

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GIT-BRANCH
+
+git branch
+git checkout -b feature-funcao-inss
+git checkout -b fix-funcao-inss
+git checkout main
+git checkout feature-funcao-inss
+git log
--- git.py
+++ git.py
@@ -1,3 +1,10 @@
+# git branch
+# git checkout -b feature-funcao-inss
+# git checkout -b fix-funcao-inss
+# git checkout main
+# git checkout feature-funcao-inss
+# git branch -d
+
 import os
 
 os.system("cls || clear")
